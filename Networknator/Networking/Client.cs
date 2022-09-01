375782afd654b893e2df4c9e8d0eaf34e05051cd
TheGBO <gabrielthejota@gmail.com>
2022-09-01 14:19:40 -0300

    added isrunning and stop on client

--- Networknator/Networking/Client.cs
+++ Networknator/Networking/Client.cs
@@ -10,6 +10,7 @@ namespace Networknator.Networking
         public static string IP { get; set; } = "127.0.0.1";
         public static int Port { get; set; } = 8090;
         public static event Action OnConnected;
+        public static bool IsRunning { get; set; } = false;
         public static TCP tcp;
         public static PacketHandlers packetHandlers = new PacketHandlers();
 
@@ -19,6 +20,13 @@ namespace Networknator.Networking
             tcp.OnConnectedAsClient += () => OnConnected?.Invoke();
             tcp.OnData += (id, data) => packetHandlers.HandlePacket(id, data);
             tcp.ConnectClient(ip, port);
+            IsRunning = true;
+        }
+
+        public static void Stop()
+        {
+            tcp = null;
+            IsRunning = false;
         }
 
         public static void SendTCPData<T>(T packet) => SendTCPData(PacketSerializer.Serialize<T>(packet));
